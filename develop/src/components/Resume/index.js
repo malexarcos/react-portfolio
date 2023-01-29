0e96a56fced4cf6ec5716a02cd145f85b87e638b
malexarcos <110743192+malexarcos@users.noreply.github.com>
2023-01-29 14:52:50 -0600

    changes in contact

--- develop/src/components/Resume/index.js
+++ develop/src/components/Resume/index.js
@@ -6,19 +6,18 @@ function Resume() {
              <p>
                 Download my <a href="https://www.linkedin.com/">resume</a>
               </p>
-              <h3>Front-end Proficiencies</h3>
+              <h3>Front-end Skills</h3>
               <ul className="skills">
                  <li>HTML</li>
                   <li>CSS</li>
                     <li>JavaScript</li>
                    <li>jQuery</li>
-                      <li>responsive design</li>
                       <li>React</li>
                       <li>Bootstrap</li>
                  </ul>
-                  <h3>Back-end Proficiencies</h3>
+                  <h3>Back-end Skills</h3>
                   <ul className="skills">
-                    <li>APIs</li>
+                    <li>API</li>
                     <li>Node</li>
                     <li>Express</li>
                     <li>MySQL, Sequelize</li>
