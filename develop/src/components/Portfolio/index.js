8af8ec99828bf33062d977f20507282ee2f0de63
malexarcos <110743192+malexarcos@users.noreply.github.com>
2023-02-01 16:38:22 -0600

    added code and files

--- develop/src/components/Portfolio/index.js
+++ develop/src/components/Portfolio/index.js
@@ -1,11 +1,64 @@
 import React, { useState } from 'react';
 
 function Portfolio() {
-    return (
-        <div>
-            <h2> This is the portfolio</h2>
-        </div>
-    )
+return (
+    <div>
+        <h2> This is the portfolio</h2>
+    </div>
+)
 }
+// function Portfolio() {
+//     const [portfolio] = useState([
+//         {
+//           name: 'calender',
+//           description: 'HTML/CSS/JavaScript',
+//           link: "https://malexarcos.github.io/calendar/",
+//           repo: "https://github.com/malexarcos/calendar"
+//         },
+//         // {
+//         //   name: 'pastel-puzzels',
+//         //   description: 'MERN Stack',
+//         //   link: "https://github.com",
+//         //   repo: "https://github.com"
+//         // },
+//         // {
+//         //   name: 'run-buddy',
+//         //   description: 'HTML/CSS',
+//         //   link: "https://github.com",
+//         //   repo: "https://github.com"
+//         // },
+//         // {
+//         //   name: 'led-wall',
+//         //   description: 'Node/IoT',
+//         //   link: "https://github.com",
+//         //   repo: "https://github.com"
+//         // },
+//         // {
+//         //   name: 'calculator',
+//         //   description: 'React/JavaScript/CSS',
+//         //   link: "https://github.com",
+//         //   repo: "https://github.com"
+//         // },
+//       ]);
+
+//       return (
+//         <div>
+//           <div className="flex-row">
+//             {.map((portfolio, idx) => (
+//               <Portfolio
+//                 portfolio={portfolio}
+//                 key={"portfolio" + idx}
+//               />
+//             ))}
+//           </div>
+//         </div>
+//       );
+// }
 
 export default Portfolio;
+
+// return (
+//     <div>
+//         <h2> This is the portfolio</h2>
+//     </div>
+// )
